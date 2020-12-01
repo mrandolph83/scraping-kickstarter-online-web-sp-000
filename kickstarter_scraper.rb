cbd99c59232a420a9e6a19edd223a526251ab5f9
Matt Smith <mrandolph83@gmail.com>
2020-12-01 17:12:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,16 +18,22 @@ def create_project_hash
 
    projects = {}
 
-  # binding.pry
+
 
   # Iterate through the projects
     kickstarter.css("li.project.grid_4").each do |project|
   # Makes each project title a key, and the value is another 
-  # hash with each of our other data points as keys.
+  # hash with each of our other data points as keys. 
+  # to_sym method. Remember that symbols make better hash 
+  # keys than strings.
      title = project.css("h2.bbcard_name strong a").text
-     projects[title.to_sym] = {}
+     projects[title.to_sym] = {:image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i}
    end
 
  # return the projects hash
  projects
 end
+
